b70a2e5833fba1f4a3783128bcf6ebbfad07e8d1
Aqufa <faiyazaquib28@gmail.com>
2020-08-18 22:01:24 +0530

    changes made for heroku

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
+const { Composer } = require('micro-bot');
 const Telegraf = require('telegraf');
 const axios = require('axios');
 const request = require('request');
 const getRestaurants = require('./utils/getRestaurants');
 
 
-const bot = new Telegraf('1233420128:AAHliiaWVLSev3N41YW__l4Y8KPvcVP25U8');
+//const bot = new Telegraf('1233420128:AAHliiaWVLSev3N41YW__l4Y8KPvcVP25U8');
+const bot = new Composer;
 
 
 
 bot.on('text', ctx => {
-    // ctx object holds the Update object from Telegram API
-    // So you can use everything you see there
+    
+    const city = ctx.message.text;
   
-    // get the text message sent by user
-    const subreddit = ctx.message.text;
-  
-    // GET the data from Reddit API
-    getRestaurants(subreddit, (error, data) =>{
+    
+    getRestaurants(city, (error, data) =>{
         ctx.reply(data)
     })
   });
 
-bot.launch();
+//bot.launch();
+const bot = new Composer
 
 
 
