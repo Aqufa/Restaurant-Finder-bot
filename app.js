1117fd7a0b7819f3666e4848e3281425615ee6be
Aqufa <faiyazaquib28@gmail.com>
2020-08-19 17:12:24 +0530

    line spacing and start message added

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const getRestaurants = require('./utils/getRestaurants');
 const bot = new Telegraf('1233420128:AAHliiaWVLSev3N41YW__l4Y8KPvcVP25U8');
 //const bot = new Composer;
 
-
+bot.start(ctx =>{
+  ctx.reply(`Hello ${ctx.from.first_name}`);
+  ctx.reply('You can search restaurants in your location.\neg: type `saket, delhi`')
+})
 
 bot.on('text', ctx => {
     
@@ -19,7 +22,7 @@ bot.on('text', ctx => {
       if(error){
         return ctx.reply('Sorry this loction is not in my directory!! Please try another one')
       }
-        ctx.reply('Name: '+ data.name + '\nLocation: '+data.locate+ '\nmenu-Link: '+data.menuLink)
+        ctx.reply('Name: '+ data.name + '\n\nLocation: '+data.locate+ '\n\nMenu-link: '+data.menuLink)
     })
   });
 
